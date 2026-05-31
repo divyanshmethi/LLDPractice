4f92e7cbc7c4df9e653901d5d5ce94c784c0aa6a
Divyansh Methi <divyanshmethi11@gmail.com>
2026-05-31 12:21:40 +0530

    Update singleFlightWithWaitGroup.go
    Added context and recovery

--- singleFlightWithWaitGroup/singleFlightWithWaitGroup.go
+++ singleFlightWithWaitGroup/singleFlightWithWaitGroup.go
@@ -1,6 +1,5 @@
-package singleFlightWithWaitGroup
-
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -27,39 +26,52 @@ func NewSingleFlight() *SingleFlight {
 	}
 }
 
-func (s *SingleFlight) Do(key string, fn func() (string, error)) (string, error) {
+func (s *SingleFlight) Do(ctx context.Context, key string, fn func(ctx context.Context) (string, error)) (string, error) {
 	s.mu.Lock()
 	if call, exists := s.calls[key]; exists {
 		s.mu.Unlock()
-		call.wg.Wait()
+		call.wg.Wait() //Does not support context -- downside
 		return call.result.val, call.result.err
 	}
 	call := &Call{wg: sync.WaitGroup{}}
 	call.wg.Add(1)
 	s.calls[key] = call
 	s.mu.Unlock()
-	call.result.val, call.result.err = fn()
+	defer s.finishCall(key, call)
+	call.result.val, call.result.err = fn(ctx)
+	return call.result.val, call.result.err
+}
+
+func (s *SingleFlight) finishCall(key string, call *Call) {
+	if r := recover(); r != nil {
+		call.result.err = fmt.Errorf("panic: %v", r)
+	}
 	call.wg.Done()
 	s.mu.Lock()
 	delete(s.calls, key)
 	s.mu.Unlock()
-	return call.result.val, call.result.err
 }
 
-func expensiveFunction() (string, error) {
+func expensiveFunction(ctx context.Context) (string, error) {
 	// Simulate an expensive operation
-	time.Sleep(2 * time.Second)
-	return "expensive result", nil
+	select {
+	case <-time.After(2 * time.Second):
+		return "user-profile-data", nil
+	case <-ctx.Done():
+		return "", ctx.Err()
+	}
 }
 
 func Master() {
 	var wg sync.WaitGroup
 	newFlight := NewSingleFlight()
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func(routineIndex int) {
 			defer wg.Done()
-			res, err := newFlight.Do("user123", expensiveFunction)
+			res, err := newFlight.Do(ctx, "user123", expensiveFunction)
 			if err != nil {
 				fmt.Printf("error with goroutine %d, err: %v\n", routineIndex, err.Error())
 				return
